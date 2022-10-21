357a65a512c82a709860691d0e7ae1dff4719606
soufianejebbari <soufiane.jebbari@dokeop.com>
2022-10-21 15:53:18 +0200

    display cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,28 +1,48 @@
 let cart = JSON.parse(localStorage.getItem("cart"));
 console.log(cart);
 
-for (let e of cart) {
-    const cartHtml =
-        `<article class="cart__item" data-id="${e.id}" data-color="${e.color}">
-    <div class="cart__item__img">
-      <img src="../images/product01.jpg" alt="Photographie d'un canapé">
-    </div>
-    <div class="cart__item__content">
-      <div class="cart__item__content__description">
-        <h2>${e.name}</h2>
-        <p>Vert</p>
-        <p>42,00 €</p>
-      </div>
-      <div class="cart__item__content__settings">
-        <div class="cart__item__content__settings__quantity">
-          <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${e.quantity}">
-        </div>
-        <div class="cart__item__content__settings__delete">
-          <p class="deleteItem">Supprimer</p>
-        </div>
-      </div>
-    </div>
-  </article>`;
-    document.getElementById("cart__items").insertAdjacentHTML('afterbegin', cartHtml);
+async function GetProductById (productId) {
+    return fetch("http://localhost:3000/api/products/" + productId)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (response) {
+            return response;
+        })
+        .catch((err) => {
+            // Une erreur est survenue
+            console.log("erreur");
+        });
 }
+
+async function displayCart() {
+    for (e of cart) {
+        const product = await GetProductById(e.id);
+        console.log(product);
+        const cartHtml =
+            `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+      <div class="cart__item__img">
+        <img src="${product.imageUrl}" alt="${product.altTxt}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${product.name}</h2>
+          <p>${e.color}</p>
+          <p>${e.price}€</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : ${e.quantity}</p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${e.quantity}">
+          </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
+          </div>
+        </div>
+      </div>
+    </article>`;
+        document.getElementById("cart__items").insertAdjacentHTML('afterbegin', cartHtml);
+    }
+}
+
+displayCart();
