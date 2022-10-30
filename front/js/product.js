04bb77c5e1a7b1af4db1674dc68153a47cef3451
soufianejebbari <soufiane.jebbari@dokeop.com>
2022-10-30 09:59:48 +0100

    Fix color and quantity validation when adding product to cart

--- front/js/product.js
+++ front/js/product.js
@@ -1,77 +1,101 @@
-// Get param from window url with search params
-
-const GetParamFromUrl = (url, param) => {
+const getParamFromUrl = (url, param) => {
     let newUrl = new URL(url);
-    let search_params = new URLSearchParams(newUrl.search);
-    let paramValue = search_params.get(param);
+    let searchParams = new URLSearchParams(newUrl.search);
+    let paramValue = searchParams.get(param);
     return paramValue;
 }
 
-// Get info for the current product (with param id)
-let window_url = window.location.href;
-const productId = GetParamFromUrl(window_url, 'id');
-const id_api = `http://localhost:3000/api/products/${productId}`;
+let windowUrl = window.location.href;
+const productId = getParamFromUrl(windowUrl, 'id');
+const productAPI = `http://localhost:3000/api/products/${productId}`;
 
-const InsertProduct = () => {
-    fetch(id_api)
-        .then(function (res) {
+const fetchProduct = async (url) => {
+    await fetch(url)
+        .then(function(res) {
             if (res.ok) {
                 return res.json();
             }
         })
-        .then(function (product) {
-            document.getElementsByClassName("item__img")[0].insertAdjacentHTML('afterbegin', `<img
-           src="${product.imageUrl}"
-           alt="${product.altTxt}"/>`);
-            document.getElementById("title").insertAdjacentHTML('afterbegin', `${product.name} `);
-            document.getElementById("price").insertAdjacentHTML('afterbegin', `${product.price} `);
-            document.getElementById("description").insertAdjacentHTML('afterbegin', `${product.description} `);
-            for (let c in product.colors) {
-                document.getElementById("colors").insertAdjacentHTML('beforeend', `<option value="${product.colors[c]}">${product.colors[c]}</option>`)
-            }
+        .then(function(value) {
+            displayProduct(value);
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+};
 
-        });
+const displayProduct = async (product) => {
+    document.getElementsByClassName("item__img")[0].insertAdjacentHTML('afterbegin', `<img
+          src="${product.imageUrl}"
+          alt="${product.altTxt}"/>`);
+    document.getElementById("title").insertAdjacentHTML('afterbegin', `${product.name} `);
+    document.getElementById("price").insertAdjacentHTML('afterbegin', `${product.price} `);
+    document.getElementById("description").insertAdjacentHTML('afterbegin', `${product.description} `);
+    for (let c in product.colors) {
+        document.getElementById("colors").insertAdjacentHTML('beforeend', `<option value="${product.colors[c]}">${product.colors[c]}</option>`)
+    }
 }
 
-InsertProduct();
-
-const addBtn = document.getElementById("addToCart");
-addBtn.addEventListener('click', () => {
-    let item = {
-        id: productId,
-        // name: document.getElementById("title").innerHTML,
-        // price: document.getElementById("price").innerHTML,
-        color: document.getElementById("colors").value,
-        quantity: document.getElementById("quantity").value,
-        // imageUrl: document.getElementsByClassName("item__img").src,
-        // altTxt: document.getElementsByClassName("item__img").alt,
+const colorValid = (color) => {
+    if (color === "") {
+        alert("Veuillez sélectionner une couleur");
+    } else {
+        return true;
     }
-    // Initialisation local storage
-    // localStorage.clear();
-    const myCart = JSON.parse(localStorage.getItem("cart"));
-    // console.log(myCart);
-    // if myCart is not empty
-    if (myCart) {
-        let findItem = myCart.find((element) => element.id === item.id && element.color === item.color);
-        // if selected product is already @ myCart
+}
+
+
+const quantityValid = (quantity) => {
+    if (quantity == 0 || quantity == "" || quantity > 100) {
+        alert("Veuillez choisir une quantité comprise entre 1 et 100");
+    } else {
+        return true;
+    }
+}
+
+const saveCart = (cart) => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+const checkCart = (item, cart) => {
+    // If cart exists we look for identical product (same color and id)
+    if (cart) {
+        let findItem = cart.find((element) => element.id === item.id && element.color === item.color);
+        // If we find identical product, we increment quantity
         if (findItem) {
             let newQuantity = parseInt(item.quantity) + parseInt(findItem.quantity);
             findItem.quantity = newQuantity;
-            localStorage.setItem("cart", JSON.stringify(myCart));
-            console.log("hi");
-            // if selected product is not @ myCart
+            saveCart(cart);
+            // If product is not already in cart we add it to cart
         } else {
-            myCart.push(item);
-            localStorage.setItem("cart", JSON.stringify(myCart));
-            console.log("hello");
+            cart.push(item);
+            saveCart(cart);
         }
-
-        // if myCart is empty
     } else {
-        let myCart = [];
-        myCart.push(item);
-        localStorage.setItem("cart", JSON.stringify(myCart));
-        console.log("coucou");
+        let cart = [];
+        cart.push(item);
+        saveCart(cart);
     }
-    console.log(myCart);
-})
+}
+
+const redirectToCart = () => {
+    window.location.href = "cart.html"
+}
+
+const addToCart = () => {
+    let myItem = {
+        id: productId,
+        color: document.getElementById("colors").value,
+        quantity: document.getElementById("quantity").value,
+    }
+    const myCart = JSON.parse(localStorage.getItem("cart"));
+
+    if (colorValid(myItem.color) && quantityValid(myItem.quantity)) {
+        checkCart(myItem, myCart);
+        redirectToCart();
+    }
+}
+
+fetchProduct(productAPI);
+const addBtn = document.getElementById("addToCart");
+addBtn.addEventListener('click', addToCart);
